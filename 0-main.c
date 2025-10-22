81f9010ad780ce55a1d285f55255242b135d1311
skicodes <skalrashed02@hotmail.com>
2025-10-22 14:03:20 +0000

    Apply Betty style fixes to main.c

--- 0-main.c
+++ 0-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
-/* Prototype for your function */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+/**
+ * main - Entry point for testing binary_tree_node
+ *
+ * Return: 0 on success, 1 on failure
+ */
 int main(void)
 {
     binary_tree_t *root;
@@ -11,8 +13,8 @@ int main(void)
     root = binary_tree_node(NULL, 98);
     if (root == NULL)
     {
-	printf("Failed to create root node\n");
-	return (1);
+        printf("Failed to create root node\n");
+        return (1);
     }
 
     printf("Root node created with value: %d\n", root->n);
