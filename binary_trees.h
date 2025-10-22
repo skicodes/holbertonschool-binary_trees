2959ec9e323dccf9c62fefd5e93c7bfde343ce13
skicodes <skalrashed02@hotmail.com>
2025-10-22 13:38:36 +0000

    Add function to create a new binary tree node - By Sarah

--- binary_trees.h
+++ binary_trees.h
@@ -4,14 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
 struct binary_tree_s
 {
     int n;
@@ -21,5 +13,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* Prototype for your function */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
